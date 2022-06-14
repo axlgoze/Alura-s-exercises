fcae90090c9978ba64d2b0de502feec999085d05
Axel Reyes <rbaxel.f@gmail.com>
2022-06-14 11:38:49 -0500

    agregnado primera prueba

---
+++ test/basictest.test.js
@@ -0,0 +1,6 @@
+describe("Test suite for FizzBuzz validation", () =>{
+    test("0) case Dummy", () =>{
+        const resultOfSomething = 16+2;
+        expect(resultOfSomething).toBe(18);
+    });
+});
